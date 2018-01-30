cce4d7bbd2655159c1a04511022291481ffc684a
Albarn <elrib7vj@gmail.com>
2018-01-30 22:03:33 +0200

    lab1 full

--- root/src/com/company/Main.java
+++ root/src/com/company/Main.java
@@ -20,11 +20,30 @@ public class Main {
         return res;
     }
 
-    //основной метод программы
-    //Вариант 24. Задана вещественная  матрица размером m x n.
-    //Упорядочить ее строки по неубыванию их наибольших элементов.
-    public static void main(String[] args) {
-	// write your code here
+    //сортировка матрицы по второму столбцу
+    public  static  double[][] BubbleSort(double[][] matrix, int n){
+        for(int i=0; i < n; i++) 			// i - номер прохода
+        {
+            for(int j = n-1; j > i; j-- ) 	// внутренний цикл прохода
+            {
+                //сравнение и обмен
+                if ( matrix[j-1][1] > matrix[j][1] ) {
+
+                    //обмен, меняем пару, а не один элемент
+                    double z=matrix[j-1][1];
+                    matrix[j-1][1]=matrix[j][1];
+                    matrix[j][1]=z;
+
+                    z=matrix[j-1][0];
+                    matrix[j-1][0]=matrix[j][0];
+                    matrix[j][0]=z;
+                }
+            }
+        }
+        return matrix;
+    }
+
+    public static void CommonWork(){
         //создание сканера для чтения чисел
         Scanner in = new Scanner(System.in);
 
@@ -34,58 +53,23 @@ public class Main {
 
         //создание матрицы, ввод элементов
         double nums[][]=new double[n][m];
-        //максимальные элементы строк матрицы nums
-        //по ним произойдет сортировка строк первой матрицы
-        //матрица содержит номер строки и максимальный элемент
-        double maxElements[][]=new double[n][2];
         for(int i=0;i<n;i++)
         {
-
-            //во время чтения попутно
-            //вычисляем максимальный элемент
-            double max=0;
-
             //чтение строки
             for(int j=0;j<m;j++)
             {
-
-                //сравнение максимального элемента
-                double val=in.nextDouble();
-                if(j==0) max=val;
-                else if(max<val)max=val;
-
                 //запись в исходную матрицу
-                nums[i][j]=val;
+                nums[i][j]=in.nextDouble();
             }
-
-            //переход на новую строку для чтения следующей строки
-            System.out.print("\n");
-            //запись в матрицу максимального элемента
-            //только что прочитанной строки
-            maxElements[i][0]=i;
-            maxElements[i][1]=max;
         }
 
+        //максимальные элементы строк матрицы nums
+        //по ним произойдет сортировка строк первой матрицы
+        //матрица содержит номер строки и максимальный элемент
+        double maxElements[][]=FindMaxesInRows(nums,n,m);
 
         //сортировка пузырьком матрицы maxElements
-        for(int i=0; i < n; i++) 			// i - номер прохода
-        {
-            for(int j = n-1; j > i; j-- ) 	// внутренний цикл прохода
-            {
-                //сравнение и обмен
-                if ( maxElements[j-1][1] > maxElements[j][1] ) {
-
-                    //обмен, меняем пару, а не один элемент
-                    double z=maxElements[j-1][1];
-                    maxElements[j-1][1]=maxElements[j][1];
-                    maxElements[j][1]=z;
-
-                    z=maxElements[j-1][0];
-                    maxElements[j-1][0]=maxElements[j][0];
-                    maxElements[j][0]=z;
-                }
-            }
-        }
+        maxElements=BubbleSort(maxElements,n);
 
         //новая строка для вывода результата
         System.out.print("\n");
@@ -107,5 +91,12 @@ public class Main {
             //новая строка
             System.out.print("\n");
         }
+    }
+
+    //основной метод программы
+    //Вариант 24. Задана вещественная  матрица размером m x n.
+    //Упорядочить ее строки по неубыванию их наибольших элементов.
+    public static void main(String[] args) {
+        CommonWork();
     }
 }
--- root/src/com/company/MainTest.java
+++ root/src/com/company/MainTest.java
@@ -1,6 +1,120 @@
 package com.company;
-import com.company.Main;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class MainTest {
 
+    //данные для тестирования:
+    //матрица и ее размеры
+    public static double[][] matrix;
+    public static int n,m;
+
+    @BeforeAll
+    //генерация случайных данных перед каждым тестом
+    public static void GenData(){
+        Random f=new Random(LocalDateTime.now().getNano());
+        n=f.nextInt(20);
+        m=f.nextInt(20);
+
+        matrix=new double[n][m];
+        for(int i=0;i<n;i++)
+            for(int j=0;j<m;j++)
+                matrix[i][j]=f.nextDouble();
+    }
+
+    //конструктор по умолчанию
+    public MainTest(){
+
+    }
+
+    @Test
+    //проверка выхода за диапазон для метода
+    //Main.FindMaxesInRows()
+    public void IndexOutOfBounds1(){
+        try{
+            //задаем размеры, большие исходных
+            Main.FindMaxesInRows(matrix,n+1,m+2);
+        }
+        catch (ArrayIndexOutOfBoundsException ex){
+            //если сгенерировано правильное исключение
+            //выходим, иначе тест не пройден
+            return;
+        }
+        fail("");
+    }
+
+    @Test
+    //проверка на отрицательный индекс
+    public void NegativeRange(){
+        try{
+            //задаем отрицательные размеры
+            //в данном методе будет попытка создать
+            //массив с такими размерами
+            Main.FindMaxesInRows(matrix,-1,-1);
+        }
+        //исключение отрицательного размера массива
+        catch (NegativeArraySizeException ex){
+            return;
+        }
+        fail("");
+    }
+
+    @Test
+    //выход за диапазон
+    //в методе сортировки(по строкам)
+    public void IndexOutOfBounds2(){
+        try{
+            Main.BubbleSort(matrix,n+1);
+        }
+        catch (ArrayIndexOutOfBoundsException ex){
+            return;
+        }
+        fail("");
+    }
+
+    @Test
+    //выход за диапазон
+    //в методе сортировки(по столбцам)
+    public void IndexOutOfBounds3(){
+        //для выхода за границы создадим массив
+        //из одного столбца(массив сортируется по второму столбцу)
+        double value[][]=new double[n][1];
+        for(int i=0;i<n;i++)
+            value[i][0]=matrix[i][0];
+        try{
+            Main.BubbleSort(value,n);
+        }
+        catch (ArrayIndexOutOfBoundsException ex){
+            return;
+        }
+        fail("");
+    }
+
+    @Test
+    //проверка правильности нахождения
+    //максимальных значений в строках матрицы
+    public void MaxesInRows(){
+        double maxes[][]=Main.FindMaxesInRows(matrix,n,m);
+        for(int i=0;i<n;i++)
+            for(int j=0;j<m;j++)
+                //сравниваем каждый элемент строки с максимальным
+                if(matrix[i][j]>maxes[i][1])
+                    fail("");
+    }
+
+    @Test
+    //проверка порядка элементов после сортировки
+    public void SortOrder(){
+        double maxes[][]=Main.FindMaxesInRows(matrix,n,m);
+        Main.BubbleSort(maxes,n);
+        for(int i=0;i<n-1;i++)
+            //проверка неубывания элементов второго столбца
+            if(maxes[i][1]>maxes[i+1][1])
+                fail("");
+    }
 }
